aa261653b13e6e452d06d5bba9363f5aa01a08ac
sumit chauhan <chauhansumit06010@gmail.com>
2026-03-14 18:57:46 +0530

    datatypes add

---
+++ Java/basics/datatypes/Datatypes.java
@@ -0,0 +1,9 @@
+package Java.basics.datatypes;
+class Datatypes{
+    public static void main(String[] arr){
+        //byte short int long
+        byte a = 10;
+        System.out.println("a = "+ a);
+        System.out.print(Byte.MIN_VALUE);
+    }
+}
