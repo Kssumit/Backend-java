eb237c66315e5918996cfe3bf6fe21be4eb9f418
sumit chauhan <chauhansumit06010@gmail.com>
2026-03-14 20:49:13 +0530

    arthmetic operator add

---
+++ Java/basics/Day02operator/arithmetic/Operator.java
@@ -0,0 +1,7 @@
+package Java.basics.Day02operator.arithmetic;
+
+public class Operator {
+    public static void main(String[] arr){
+        // +, -, *, /, %, ++, -- 
+    }
+}
--- Java/basics/operator/Operator.java
+++
@@ -1,7 +0,0 @@
-package Java.basics.operator;
-
-public class Operator {
-    public static void main(String[] arr){
-        System.out.println(3+4);
-    }
-}
