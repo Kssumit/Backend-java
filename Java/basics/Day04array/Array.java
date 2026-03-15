7e48268cfbaa9489a9ea5128d13df374cff87f90
sumit chauhan <chauhansumit06010@gmail.com>
2026-03-15 16:33:00 +0530

    array add

---
+++ Java/basics/Day04array/Array.java
@@ -0,0 +1,32 @@
+package Java.basics.Day04array;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+
+        int[] arr1 = {1,2,454,5};
+        int len = arr1.length;
+
+        for(int num:arr){
+            System.out.println(num);
+        }
+
+        int[][] arr2 = new int[3][3];
+        
+        int[][] matrix= {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        for (int i = 0; i<matrix.length; i++){
+            for (int j = 0; j<matrix[i].length; j++){
+                System.out.println(matrix[i][j]);
+            }
+        }
+
+
+    }
+}
