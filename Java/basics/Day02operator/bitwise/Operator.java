377b3a63974d0e7f157d3a422347073049d2341e
sumit chauhan <chauhansumit06010@gmail.com>
2026-03-15 14:06:53 +0530

    bitwise operator add

---
+++ Java/basics/Day02operator/bitwise/Operator.java
@@ -0,0 +1,22 @@
+package Java.basics.Day02operator.bitwise;
+
+public class Operator {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(Integer.toBinaryString(a));
+        //we can use bitwise operator on byte short int long
+        //and &, or |, xor ^, not ~, left shift <<, right shift >>, unsigned right shift >>>,
+
+        //table
+        /*       &  |  ^ 
+            0 0  0  0  0  
+            1 0  0  1  1
+            0 1  0  1  1
+            1 1  1  1  0
+        */
+
+        //(uniary operator) not ~ : 1 -> 0 and 0 -> 1
+        
+
+    }
+}
