d91e0c0489ae323f431705a46331141b9c2648e6
sumit chauhan <chauhansumit06010@gmail.com>
2026-03-15 15:04:55 +0530

    string add

---
+++ Java/basics/Day03string/Test.java
@@ -0,0 +1,15 @@
+package Java.basics.Day03string;
+
+public class Test {
+    public static void main(String[] args) {
+        String a = new String("jonas");  //a refer to a different loc in heap
+        String b = new String("jonas"); // b refer to different loc 
+
+        System.out.println(a == b);  // we comparing reference here
+            //false
+        String c = "jonas"; //jonas ek string pool me jake store hogya
+        String d = "jonas";// c & d refer to a same reference
+
+        System.out.println(c == d); // true
+    }
+}
