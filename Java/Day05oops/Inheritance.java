cd13800834481113d4bfe3b729cfe616e3483c4d
sumit chauhan <chauhansumit06010@gmail.com>
2026-03-21 15:12:27 +0530

    multilevel add

---
+++ Java/Day05oops/Inheritance.java
@@ -0,0 +1,68 @@
+package Java.Day05oops;
+
+//1st grandParent constructor is called then parent then child 
+
+class GrandParent{
+    private String name;
+    private int age;
+
+    private boolean superPower;
+
+    public GrandParent() {
+        superPower = false;
+        System.out.println("Grand parent constructor is called");
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+    public void show(){
+        System.out.println("details about person");
+    }
+
+}
+
+class Parent extends GrandParent{
+    public Parent(){
+        System.out.println("Parent constructor is called");
+    }
+
+    public void show(){
+        System.out.println("parent");
+    }
+}
+
+class Child extends Parent{
+    public Child(){
+        System.out.println("Child constructor is called");
+    }
+
+        public void show(){
+        System.out.println("child");
+    }
+}
+
+
+
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.setName("sumit");
+        child.setAge(18);
+        System.out.println(child.getAge() + " " + child.getName());
+        child.show();
+    }
+}
