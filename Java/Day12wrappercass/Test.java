2bd8ebaeae6835bb579959ea7f8eb334cf80bf12
sumit chauhan <chauhansumit06010@gmail.com>
2026-03-25 13:54:56 +0530

    wrapper class add

---
+++ Java/Day12wrappercass/Test.java
@@ -0,0 +1,13 @@
+package Java.Day12wrappercass;
+
+//wrapper class: convert primitive datatypes into object
+
+public class Test {
+    public static void main(String[] args) {
+        //autoboxing
+        Integer a = 1;   //Integer a = Integer.valueOf(1)
+        //autounboxing
+        int b = a; //a.intValue();
+
+    }
+}
