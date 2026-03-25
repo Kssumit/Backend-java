05e3a3d0098c1e3079c18b5e26717a83451f71e2
sumit chauhan <chauhansumit06010@gmail.com>
2026-03-25 12:34:15 +0530

    throw and finally add

--- Java/Day11exceptionhandling/checkedUnchecked/ThrowExample.java
+++ Java/Day11exceptionhandling/checkedUnchecked/ThrowExample.java
@@ -3,8 +3,15 @@ package Java.Day11exceptionhandling.checkedUnchecked;
 import java.io.FileNotFoundException;
 
 public class ThrowExample {
-    public static void main(String[] args) throws FileNotFoundException{
-        method1();
+    public static void main(String[] args){
+        try{
+            method1();
+        }catch(FileNotFoundException e){
+            System.out.println("dekho dekho");
+            System.out.println(e);
+        }finally{
+            System.out.println("always run");
+        }
     }
 
     public static void method1() throws FileNotFoundException{
