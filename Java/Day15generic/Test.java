44a1ed99e0e97d0b9668c73834d29b4b64152f29
sumit chauhan <chauhansumit06010@gmail.com>
2026-03-28 13:56:09 +0530

    generic convention

--- Java/Day15generic/Test.java
+++ Java/Day15generic/Test.java
@@ -1,7 +1,14 @@
 package Java.Day15generic;
 
 //generic types allow you to define a class, interface or method with type paramters for the data types they will work with.
+/*
+T : single emlement
+K: key (use in map)
+V: value (use in map)
+E: Element (used in maps)
+N: Number
 
+*/
 public class Test {
     
 }
