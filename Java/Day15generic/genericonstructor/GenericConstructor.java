989ae620b7c0cd47af0adb8f2fc839052d15a0ab
sumit chauhan <chauhansumit06010@gmail.com>
2026-03-28 14:35:56 +0530

    generic constructor add

--- Java/Day15generic/genericonstructor/GenericConstructor.java
+++ Java/Day15generic/genericonstructor/GenericConstructor.java
@@ -4,4 +4,9 @@ package Java.Day15generic.genericonstructor;
 public class GenericConstructor {
     public <T> GenericConstructor(T val){}
     public <T extends Number> GenericConstructor(T val){}
+
+    public static void main(String[] args) {
+        GenericConstructor obj = new GenericConstructor(1);
+        GenericConstructor obj1 = new GenericConstructor("hero");
+    }
 }
