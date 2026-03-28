635b85a59fb9194d8d8a4c8afeb5aa62209671dc
sumit chauhan <chauhansumit06010@gmail.com>
2026-03-28 13:41:16 +0530

    generic add

---
+++ Java/Day15generic/Test.java
@@ -0,0 +1,7 @@
+package Java.Day15generic;
+
+//generic types allow you to define a class, interface or method with type paramters for the data types they will work with.
+
+public class Test {
+    
+}
