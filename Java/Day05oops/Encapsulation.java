ccafb8ca2b81236d1a649e0ea11ae52c6f874700
sumit chauhan <chauhansumit06010@gmail.com>
2026-04-05 14:06:04 +0530

    record data class

--- Java/Day05oops/Encapsulation.java
+++ Java/Day05oops/Encapsulation.java
@@ -1,5 +1,7 @@
 package Java.Day05oops;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 class Cat{
     private String name;
@@ -10,8 +12,43 @@ class Cat{
     }
 
 }
+
+record Point(double x, double y) {} 
+
 public class Encapsulation {
     public static void main(String[] aa){
+
+        var p = new Point(2,5);
+        double slope = p.x()/p.y();
+        System.out.println(slope);
+
+        double slope1 = switch(p){
+            case Point(var a, var b) -> b/a;
+        };
+        System.out.println(slope1);
+
+
+
         //In this properties are private and methods are public
+        LocalDate date = LocalDate.of(2026, 4, 1);
+        date = date.plusDays(1);
+        // System.out.println(date);
+
+        // while (date.getMonthValue()==1) {
+        //     System.out.printf("%4d",date.getDayOfMonth());
+        //     date = date.plusDays(1);
+        // }
+        
+        DayOfWeek weekDay = date.getDayOfWeek();
+        // System.out.println(weekDay);
+        int value = date.getDayOfWeek().getValue();
+
+        for(int i = 1; i<=7 ; i++){
+            System.out.printf("%s  ",weekDay);
+        }
+
+
+        
+        }
     }
-}
+
