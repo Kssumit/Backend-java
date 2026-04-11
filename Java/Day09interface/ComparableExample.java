9366cb6620add505453b6d85e80060bce392ddc2
sumit chauhan <chauhansumit06010@gmail.com>
2026-04-11 14:03:02 +0530

    comparable interface

---
+++ Java/Day09interface/ComparableExample.java
@@ -0,0 +1,49 @@
+package Java.Day09interface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+class Student implements Comparable<Student> {
+    int rollNo;
+    String name;
+    Student(int roll, String name){
+        this.rollNo = roll;
+        this.name = name;
+    }
+
+    public int compareTo(Student s){
+        return this.rollNo - s.rollNo; 
+    }
+
+    public String toString(){
+        return rollNo + " " + name;
+    }
+}
+
+public class ComparableExample{
+    public static void main(String[] args) {
+        Student s1 = new Student(1, "sumit");
+        Student s2 = new Student(2, "sumit");
+        Student s3 = new Student(3, "sumit");
+        Student s4 = new Student(4, "sumit");
+
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(s1);
+        list.add(s2);
+        list.add(s3);
+        list.add(s4);
+
+        Student[] arr = {s1,s2,s3,s4};
+        Arrays.sort(arr);
+
+        for(Student s:arr){
+            System.out.println(s.rollNo+" "+s.name);
+        }
+
+        Collections.sort(list);
+        System.out.print(list);
+
+    }
+}
