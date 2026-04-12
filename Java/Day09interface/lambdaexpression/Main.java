c2da786e8e8c146d72969cbba2abdd7d2a222694
sumit chauhan <chauhansumit06010@gmail.com>
2026-04-12 16:30:55 +0530

    higher order function

--- Java/Day09interface/lambdaexpression/Main.java
+++ Java/Day09interface/lambdaexpression/Main.java
@@ -1,5 +1,7 @@
 package Java.Day09interface.lambdaexpression;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.function.IntConsumer;
 
 // class Demo implements IntConsumer{
@@ -11,6 +13,15 @@ import java.util.function.IntConsumer;
 public class Main {
     public static void main(String[] args) {
         repeat(10, (i)->System.out.println("countdown "+ (i)));
+
+        String[] names = {"sumit","zero","hearo"};
+        // Arrays.sort(names,compareInDir(-1));
+
+        Arrays.sort(names,reverse(String::compareToIgnoreCase));
+
+        for(String n:names){
+            System.out.println(n);
+        }
     }
 
     // public static void repeat(int n,Runnable action){
@@ -19,6 +30,14 @@ public class Main {
 
     public static void repeat(int n, IntConsumer action){
         for(int i = 0; i<n; i++) action.accept(i);
+    }
+
+    public static Comparator<String> compareInDir(int direction){
+        return (x,y)-> direction * x.compareTo(y);
+    }
+
+    public static Comparator<String> reverse(Comparator<String> comp){
+        return (x,y)-> comp.compare(y,x);
     }
 
 }
