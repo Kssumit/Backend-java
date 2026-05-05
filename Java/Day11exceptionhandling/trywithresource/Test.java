d23cbe2cb2b5b981377fdee5b18518455da71155
sumit chauhan <chauhansumit06010@gmail.com>
2026-05-05 23:28:24 +0530

    try with resource

--- Java/Day11exceptionhandling/trywithresource/Test.java
+++ Java/Day11exceptionhandling/trywithresource/Test.java
@@ -3,9 +3,11 @@ package Java.Day11exceptionhandling.trywithresource;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         //reader ko close krna important isliye humne finally me likha hai 
         //taki always reader close hojaye
         //we can do this without finally using try with resource 
@@ -29,13 +31,32 @@ public class Test {
         //     }
         // }
 
-        try(BufferedReader reader = new BufferedReader(new FileReader("abc.txt"))){
-            String line;
-            while((line = reader.readLine()) != null){
-                 System.out.println(line);
-           }
-        }catch(IOException e){
-            System.err.println(e.getMessage());
+        // try(BufferedReader reader = new BufferedReader(new FileReader("abc.txt"))){
+        //     String line;
+        //     while((line = reader.readLine()) != null){
+        //          System.out.println(line);
+        //    }
+        // }catch(IOException e){
+        //     System.err.println(e.getMessage());
+        // }
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("my");
+        list.add("is");
+
+        try{
+            var out = new PrintWriter("output.txt");
+            for(String line:list){
+                out.println(line);
+            }
+            out.close();
+        }catch(Exception e){
+            System.out.print(e);
+        }
+
+        try(var out = new PrintWriter("output.txt")){
+
         }
     }
 }
